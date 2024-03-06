709b25d1edfaca4153532e11500d5214f2b62737
iRobot90 <amiandajesse27@gmail.com>
2024-03-06 13:05:50 +0300

    -added signup redirect to login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,3 +5,7 @@ from .views import SignUpView
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
