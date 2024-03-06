3e77d8701492b7d2e058a0875e7bfb249d8d35a8
iRobot90 <amiandajesse27@gmail.com>
2024-03-06 23:26:20 +0300

    -added password_reset _added password_change _default SMTP email redirect

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.views.generic import CreateView
 from . views import SignUpView
+
+
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
