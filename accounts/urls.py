a9a188e153a2af83151ed9958828e7edb9ba774c
iRobot90 <amiandajesse27@gmail.com>
2024-03-06 13:18:38 +0300

    redirect bug fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
-
-from .views import SignUpView
-
+from django.views.generic import CreateView
+from . views import SignUpView
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
